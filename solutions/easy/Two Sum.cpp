d657137d18c3209f3d658911c7e8f73af9e76cc9
sujeetsyadav07 <sujeet.syadav07@gmail.com>
2026-08-23 18:39:41 +0530

    Add solution for Two Sum

---
+++ solutions/easy/Two Sum.cpp
@@ -0,0 +1,18 @@
+// Title: Two Sum
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/two-sum/
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+        if(nums[i]+nums[j]==target){
+        return {i,j};
+    }
+    }
+    return{};
+};
+        int n=nums.size();
+    vector<int> twoSum(vector<int>& nums, int target) {
+        }
+    }
+
