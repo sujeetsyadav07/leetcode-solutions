687573637bc9326e58bf23cb65841b0223c06e25
sujeetsyadav07 <sujeet.syadav07@gmail.com>
2026-09-07 00:38:46 +0530

    Add solution for Single Number

---
+++ solutions/easy/Single Number.cpp
@@ -0,0 +1,16 @@
+// Title: Single Number
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       int n=nums.size();
+       int ans=0;
+       for(int i=0;i<n;i++){
+        ans^=nums[i];
+       }
+       return ans;
+    }
+};
