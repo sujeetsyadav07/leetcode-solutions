e2bd11bd24c91d482dee87ddae43306c6c589d01
Sujeet Yadav <sujeet.syadav07@gmail.com>
2026-09-08 00:44:02 +0530

    Add solution for Majority Element

---
+++ solutions/easy/Majority Element.cpp
@@ -0,0 +1,20 @@
+// Title: Majority Element
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/majority-element/
+
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+        }
+                if(nums[i]==nums[j]){
+            }
+                    count++;
+                }
+                if(count>n/2){
+                    return nums[i];
+                }
+    }
+        return -1;
+};
