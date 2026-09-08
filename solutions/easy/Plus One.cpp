02e820980a2c46cef71cccfaeecebfd2aea829ee
Sujeet Yadav <sujeet.syadav07@gmail.com>
2026-09-08 23:27:34 +0530

    Add solution for Plus One

---
+++ solutions/easy/Plus One.cpp
@@ -0,0 +1,20 @@
+// Title: Plus One
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/plus-one/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
